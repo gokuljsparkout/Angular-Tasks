02e00736dbff8de2f08e2fd3637b25b721e6bd5f
gokuljsparkout <gokul@sparkouttech.com>
2023-05-18 16:49:34 +0530

    Topic Component Created

---
+++ src/app/topic-explorer/topic-explorer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topic-explorer',
+  templateUrl: './topic-explorer.component.html',
+  styleUrls: ['./topic-explorer.component.css']
+})
+export class TopicExplorerComponent {
+
+}
